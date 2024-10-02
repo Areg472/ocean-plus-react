9f573561101f0f6a5f7e12fa420818f265524c80
Areg472 <aregazatyank@outlook.com>
2024-10-02 13:14:18 +0400

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { SpeedInsights } from '@vercel/speed-insights/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />,
-      <SpeedInsights />
+    <App />
   </StrictMode>,
 )

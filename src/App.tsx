e4d7e6677eeaf706f9600dae1b65592b68828103
Areg472 <aregazatyank@outlook.com>
2024-10-02 12:03:56 +0400

    image, demo website ready!

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,8 @@ import './App.css'
 function App() {
 
   return (
-      <body className="background">
+      <body className="body">
       <div className="Headings">
-
           <h1>The new vyond streaming website</h1>
           <h2>Welcome!</h2>
       </div>

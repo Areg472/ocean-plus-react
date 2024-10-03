9e491f92d36468a6301dd0af470c4df177951ab1
1leon <wezeria@proton.me>
2024-10-03 17:29:16 +0200

    css + jsx modifications

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,15 @@ function App() {
     return (
     <>
         <head>
-            <title>Ocean plus</title>
+            <title>Ocean+ 0.1</title>
         </head>
         <body className="body">
         <div className="Headings">
-            <h1>The new vyond streaming website</h1>
-            <h2>Made by your favorite creators!</h2>
-            <tr className="Imagesprofile">
-                <td><img className="Profileimage1" src={profile1} alt="areg"/>
-                    <div className="textprofile1"><h2>Areg</h2></div>
-                </td>
-                <td><img className="Profileimage2" src={profile2} alt="leon"/>
-                    <div className="textprofile1"><h2>1Leon</h2></div>
-                </td>
-                <td><img className="Profileimage3" src={profile3} alt="veyshal"/>
-                    <div className="textprofile1"><h2>Veyshal</h2></div>
-                </td>
-            </tr>
+            <h1>Vyond movies and shows.</h1>
+            <h2>Anywhere, anytime, for free, no restrictions.</h2>
+            <td>
+                <input className="oplusbuttons" type="button" value="Access As Guest"/>
+            </td>
         </div>
         </body>
     </>

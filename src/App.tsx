d2656296210e57bc6d91cc80ce46e6713c1d71a1
Areg472 <aregazatyank@outlook.com>
2024-10-03 18:19:58 +0400

    Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -11,17 +11,17 @@ function App() {
         </head>
         <body className="body">
         <div className="Headings">
-            <h1>The new vyond streaming website</h1>
-            <h2>Made by your favorite creators!</h2>
+            <h1 className="font-bold text-3xl leading-loose">The new vyond streaming website</h1>
+            <h2 className="font-bold text-2xl leading-loose">Made by your favorite creators!</h2>
             <tr className="Imagesprofile">
                 <td><img className="Profileimage1" src={profile1} alt="areg"/>
-                    <div className="textprofile1"><h2>Areg</h2></div>
+                    <div className="textprofile1 font-bold text-2xl"><h2>Areg</h2></div>
                 </td>
                 <td><img className="Profileimage2" src={profile2} alt="leon"/>
-                    <div className="textprofile1"><h2>1Leon</h2></div>
+                    <div className="textprofile1 font-bold text-2xl"><h2>1Leon</h2></div>
                 </td>
                 <td><img className="Profileimage3" src={profile3} alt="veyshal"/>
-                    <div className="textprofile1"><h2>Veyshal</h2></div>
+                    <div className="textprofile1 font-bold text-2xl"><h2>Veyshal</h2></div>
                 </td>
             </tr>
         </div>

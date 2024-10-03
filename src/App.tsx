c762a946460c09930c1d2693d3494af129f2ef62
Areg472 <aregazatyank@outlook.com>
2024-10-03 11:04:47 +0400

    Text on images!

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,15 @@ function App() {
             <h1>The new vyond streaming website</h1>
             <h2>Made by your favorite creators!</h2>
             <tr className="Imagesprofile">
-                <td className="Profileimage1"><img src={profile1} alt="areg"/></td>
-                <td className="Profileimage2"><img src={profile2} alt="leon"/></td>
-                <td className="Profileimage3"><img src={profile3} alt="veyshal"/></td>
+                <td><img className="Profileimage1" src={profile1} alt="areg"/>
+                    <div className="textprofile1"><h2>Areg</h2></div>
+                </td>
+                <td><img className="Profileimage2" src={profile2} alt="leon"/>
+                    <div className="textprofile1"><h2>1Leon</h2></div>
+                </td>
+                <td><img className="Profileimage3" src={profile3} alt="veyshal"/>
+                    <div className="textprofile1"><h2>Veyshal</h2></div>
+                </td>
             </tr>
         </div>
         </body>

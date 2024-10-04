3919dd3826239c47ec03c44a9e42ab938d05f900
Areg472 <aregazatyank@outlook.com>
2024-10-04 13:37:49 +0400

    mobile optimi- *sleeps*

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -9,8 +9,8 @@ export function Homepage() {
             </head>
             <body className="body">
             <div className="Headings">
-                <h1 className="leading-loose">Vyond movies and shows.</h1>
-                <h2 className="leading-loose">Anywhere, anytime, for free, no restrictions.</h2>
+                <h1 className="mb-4">Vyond movies and shows.</h1>
+                <h2 className="mb-1.5">Anywhere, anytime, for free, no restrictions.</h2>
                 <Link to="/Movies"><input className="oplusbuttons leading-loose mt-4" type="button" value="Access As Guest"/></Link>
             </div>
             </body>

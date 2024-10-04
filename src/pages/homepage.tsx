530dca8af4c715848545c50b1db0bb076d0f9c5a
Areg472 <aregazatyank@outlook.com>
2024-10-04 10:26:50 +0400

    nothing yet

---
+++ src/pages/homepage.tsx
@@ -0,0 +1,19 @@
+import './homepage.css';
+
+export function Homepage() {
+    return (
+        <>
+            <head>
+                <title>Ocean+ 0.1</title>
+            </head>
+            <body className="body">
+            <div className="Headings">
+                <h1 className="leading-loose">Vyond movies and shows.</h1>
+                <h2 className="leading-loose">Anywhere, anytime, for free, no restrictions.</h2>
+                <input className="oplusbuttons leading-loose mt-4" type="button" value="Access As Guest"/>
+            </div>
+            </body>
+        </>
+
+    )
+}

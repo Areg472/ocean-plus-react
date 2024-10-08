8b69cd4d6d3fcff0fcf805269c9c944e1ff4ac4b
Areg472 <aregazatyank@outlook.com>
2024-10-08 18:30:57 +0400

    embedus...

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,17 +1,17 @@
 import './homepage.css';
 import {Link} from "react-router-dom"
-import { HelmetProvider } from 'react-helmet-async';
+import {Helmet} from 'react-helmet';
 
 export function Homepage() {
     return (
         <>
-            <HelmetProvider>
+            <Helmet>
                 <title>Ocean+ - Homepage</title>
                 <meta name="description" content="The place for Vyond movies!"/>
                 <meta property="og:title" content="Ocean+ - Homepage"/>
                 <meta property="og:description" content="The place for Vyond movies!"/>
                 <meta property="og:image" content="https://raw.githubusercontent.com/Areg472/ocean-plus-react/refs/heads/main/src/pictures/logo.jpg"/>
-            </HelmetProvider>
+            </Helmet>
             <body className="body">
             <div className="Headings">
                 <h1 className="mb-4 leading-normal">Vyond movies and shows.</h1>

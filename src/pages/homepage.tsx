9d53c80242e16ee53a94ee7a124abe036d66388c
Areg472 <aregazatyank@outlook.com>
2024-10-08 20:22:09 +0400

    EMBED OR ELSE!

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,18 +1,25 @@
 import './homepage.css';
 import {Link} from "react-router-dom"
-import { SEO } from '../components/SEO'
+import { Helmet } from 'react-helmet';
 
 
 export function Homepage() {
     return (
 
         <>
-            <SEO title='Ocean+'
-                 description='The place for Vyond movies!'
-                 name='Ocean+'
-                 type='article'
-                 twittercard='summary_large_image'
-                 image={'https://raw.githubusercontent.com/Areg472/ocean-plus-react/refs/heads/main/src/pictures/logo.jpg'}/>
+            <Helmet>
+                <title>Ocean+ - Homepage</title>
+                <meta name="description" content='The place to watch Vyond content!'/>
+                <meta property="og:title" content='Ocean+ - Homepage'/>
+                <meta property="og:description" content='The place to watch Vyond content!'/>
+                <meta property="og:image"
+                      content='https://raw.githubusercontent.com/Areg472/ocean-plus-react/refs/heads/main/src/pictures/logo.jpg'/>
+                <meta property="og:type" content="website"/>
+                <meta name="twitter:title" content='Ocean+ - Homepage'/>
+                <meta name="twitter:description" content='The place to watch Vyond content!'/>
+                <meta name="twitter:image" content='https://raw.githubusercontent.com/Areg472/ocean-plus-react/refs/heads/main/src/pictures/logo.jpg'/>
+                <meta name="twitter:card" content="summary_large_image"/>
+            </Helmet>
             <body className="body">
             <div className="Headings">
                 <h1 className="mb-4 leading-normal">Vyond movies and shows.</h1>

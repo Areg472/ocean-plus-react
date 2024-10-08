d07907c57f7ac8ab215d79184f52f31d20d5604a
Areg472 <aregazatyank@outlook.com>
2024-10-08 13:01:45 +0400

    EMBED OR ELSE!

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -7,7 +7,9 @@ export function Homepage() {
             <head>
                 <title>Ocean+ 0.1</title>
                 <meta property="og:title" content="Ocean+ - Homepage"/>
-                <meta property="og:image" content="../pictures/logo.jpg" />
+                <meta property="og:type" content="website"/>
+                <meta property="og:url" content="https://ocean-plus-react.pages.dev/"/>
+                <meta property="og:image" content="../pictures/logo.jpg"/>
             </head>
             <body className="body">
             <div className="Headings">

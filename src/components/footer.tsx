8eeba631976b663c9a0aba2d1c8e01da806c319e
Areg472 <aregazatyank@outlook.com>
2024-10-08 12:16:25 +0400

    footer mobile fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -12,15 +12,15 @@ export function Footer()
                 </div>
                 <div className="column middle">
                     <a target="_blank" href="https://uptime.betterstack.com">
-                        <button className="buttons">
+                        <button className="buttons middle">
                                 <img className="uptimebutton"
                                      alt="Better Stack Website Monitoring"
                                      src="https://uptime.betterstack.com/assets/static_assets/badges/blue.png"/>
                         </button></a>
                     </div>
-                    <div className="column right">
+                    <div className="column">
                         <a target="_blank" href="https://www.youtube.com/@RosieBrosEntertainment">
-                             <button className="buttons">© Ocean Blue Studios, 2024</button>
+                             <button className="buttons middle">© Ocean Blue Studios, 2024</button>
                         </a>
                     </div>
                 </div>

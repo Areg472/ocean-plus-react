c269e76147819acebfd2a8a48299b5aa6412bcee
Areg472 <aregazatyank@outlook.com>
2024-10-08 21:46:56 +0400

    EMBED OR ELSE!

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,25 +1,11 @@
 import './homepage.css';
 import {Link} from "react-router-dom"
-import { Helmet } from 'react-helmet-async';
 
 
 export function Homepage() {
     return (
 
         <>
-                <Helmet>
-                <title>Ocean+ - Homepage</title>
-                <meta name="description" content='The place to watch Vyond content!'/>
-                <meta property="og:title" content='Ocean+ - Homepage'/>
-                <meta property="og:description" content='The place to watch Vyond content!'/>
-                <meta property="og:image"
-                      content='https://raw.githubusercontent.com/Areg472/ocean-plus-react/refs/heads/main/src/pictures/logo.jpg'/>
-                <meta property="og:type" content="website"/>
-                <meta name="twitter:title" content='Ocean+ - Homepage'/>
-                <meta name="twitter:description" content='The place to watch Vyond content!'/>
-                <meta name="twitter:image" content='https://raw.githubusercontent.com/Areg472/ocean-plus-react/refs/heads/main/src/pictures/logo.jpg'/>
-                <meta name="twitter:card" content="summary_large_image"/>
-                </Helmet>
 
             <body className="body">
             <div className="Headings">

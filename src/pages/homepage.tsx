364377eaa1db08e3b4b4e93a7b3a6c18e2f90544
Areg472 <aregazatyank@outlook.com>
2024-10-08 18:24:40 +0400

    embedus...

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,16 +1,17 @@
 import './homepage.css';
 import {Link} from "react-router-dom"
-import { SEO } from '../components/SEO'
+import { HelmetProvider } from 'react-helmet-async';
 
 export function Homepage() {
     return (
         <>
-            <SEO title='Ocean+ - Homepage'
-                 description='The place for Vyond movies!'
-                 name='Ocean+'
-                 type='article'
-                 image='https://raw.githubusercontent.com/Areg472/ocean-plus-react/refs/heads/main/src/pictures/logo.jpg'/>
-
+            <HelmetProvider>
+                <title>Ocean+ - Homepage</title>
+                <meta name="description" content="The place for Vyond movies!"/>
+                <meta property="og:title" content="Ocean+ - Homepage"/>
+                <meta property="og:description" content="The place for Vyond movies!"/>
+                <meta property="og:image" content="https://raw.githubusercontent.com/Areg472/ocean-plus-react/refs/heads/main/src/pictures/logo.jpg"/>
+            </HelmetProvider>
             <body className="body">
             <div className="Headings">
                 <h1 className="mb-4 leading-normal">Vyond movies and shows.</h1>

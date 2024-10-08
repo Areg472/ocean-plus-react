8c8794ae204ba0efa8321ff0a7515ccadf6c971d
Areg472 <aregazatyank@outlook.com>
2024-10-08 21:33:24 +0400

    EMBED OR ELSE!

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import HelmetWrapper from "./components/HelmetWrapper.tsx";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <HelmetWrapper>
+            <App />
+        </HelmetWrapper>
     </StrictMode>
 )

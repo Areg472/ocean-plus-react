11009e01b6cc04e6e4d46e344edfe4cdae4cff6f
Areg472 <aregazatyank@outlook.com>
2024-10-08 20:10:07 +0400

    EMBED OR ELSE!

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,11 +1,18 @@
 import './homepage.css';
 import {Link} from "react-router-dom"
+import { SEO } from '../components/SEO'
 
 
 export function Homepage() {
     return (
-        <>
 
+        <>
+            <SEO title='Ocean+'
+                 description='The place for Vyond movies!'
+                 name='Ocean+'
+                 type='article'
+                 twittercard='summary_large_image'
+                 image={'https://raw.githubusercontent.com/Areg472/ocean-plus-react/refs/heads/main/src/pictures/logo.jpg'}/>
             <body className="body">
             <div className="Headings">
                 <h1 className="mb-4 leading-normal">Vyond movies and shows.</h1>

d43cd340bf575a14f7c8dc83758cc1e1d9b92f41
Areg472 <aregazatyank@outlook.com>
2024-10-09 11:41:10 +0400

    EMBED OR ELSE!

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,16 +1,15 @@
 import {Navbar} from "./components/navbar.tsx"
 import {Outlet} from "react-router-dom"
 import {Footer} from "./components/footer.tsx"
-import { HelmetProvider } from 'react-helmet-async';
 
 export function Layout() {
     return (
-        <HelmetProvider>
+        <>
             <Navbar/>
             <main>
                 <Outlet/>
             </main>
             <Footer/>
-        </HelmetProvider>
+        </>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
+    <HelmetProvider>
         <App />
-    </StrictMode>
+    </HelmetProvider>
 )

a5c9cba200f2c618625846817390ba912c56884f
Areg472 <aregazatyank@outlook.com>
2024-10-09 12:06:29 +0400

    EMBED OR ELSE!

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,7 +1,6 @@
 import './homepage.css';
 import {Link} from "react-router-dom"
-import {Helmet} from "react-helmet-async";
-
+import MetaTags from "../components/MetaTags.tsx";
 
 export function Homepage() {
     return (
@@ -9,12 +8,12 @@ export function Homepage() {
         <>
 
             <body className="body">
-            <Helmet>
-                <meta property="og:url" content="reactus.oceanbluestream.com"/>
-                <meta property="og:type" content="website"/>
-                <meta property="og:title" content="Ocean+ - Homepage"/>
-                <meta property="og:description" content="The place for Vyond content"/>
-            </Helmet>
+            <MetaTags
+                title='Ocean+ - Homepage'
+                description='The place to watch Vyond content!'
+                image='https://raw.githubusercontent.com/Areg472/ocean-plus-react/refs/heads/main/src/pictures/logo.jpg'
+                name='Ocean+'
+            />
             <div className="Headings">
                 <h1 className="mb-4 leading-normal">Vyond movies and shows.</h1>
                 <h2 className="mb-1.5 leading-normal">Anywhere, anytime, for free, no restrictions.</h2>

c29063ef4c971406f4e0af8a860c5d9e7b7644f0
Areg472 <aregazatyank@outlook.com>
2024-10-09 12:09:47 +0400

    EMBED OR ELSE!

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -5,22 +5,21 @@ import MetaTags from "../components/MetaTags.tsx";
 export function Homepage() {
     return (
 
-        <>
-
-            <body className="body">
+        <div>
             <MetaTags
                 title='Ocean+ - Homepage'
                 description='The place to watch Vyond content!'
                 image='https://raw.githubusercontent.com/Areg472/ocean-plus-react/refs/heads/main/src/pictures/logo.jpg'
                 name='Ocean+'
             />
+            <body className="body">
             <div className="Headings">
                 <h1 className="mb-4 leading-normal">Vyond movies and shows.</h1>
                 <h2 className="mb-1.5 leading-normal">Anywhere, anytime, for free, no restrictions.</h2>
                 <Link to="/Movies"><input className="oplusbuttons leading-loose mt-4" type="button" value="Access As Guest"/></Link>
             </div>
             </body>
-        </>
+        </div>
 
     )
 }

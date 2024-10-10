4cc7b40985e5af36c35b427c51393e5ffcb85dd6
Areg472 <aregazatyank@outlook.com>
2024-10-10 09:29:41 +0400

    embedus...

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,15 +5,17 @@ import './navbar.css'
 export function Navbar() {
     return (
         <>
-            <Link to="/">
-                <button className="button leading-loose">Home</button>
-            </Link>
-            <Link to="Movies">
-                <button className="button leading-loose">Movies</button>
-            </Link>
-            <Link to="/Contact-Us">
-                <button className="button leading-loose">Contact Us</button>
-            </Link>
+            <div className="navbar">
+                <Link to="/">
+                    <button className="button leading-loose">Home</button>
+                </Link>
+                <Link to="Movies">
+                    <button className="button leading-loose">Movies</button>
+                </Link>
+                <Link to="/Contact-Us">
+                    <button className="button leading-loose">Contact Us</button>
+                </Link>
+            </div>
         </>
     )
 }

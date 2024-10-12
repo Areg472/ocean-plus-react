77bf97d5c329c617dcfdc282c37465769f8d989f
Areg472 <aregazatyank@outlook.com>
2024-10-12 16:48:18 +0400

    rollback

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -14,7 +14,7 @@ export function Homepage() {
             <body className="body">
             <div className="Headings">
                 <h1 className="mb-4 leading-normal">Vyond movies and shows.</h1>
-                <h2 className="mb-1.5 leading-normal">Anywhere, anytime, for free, no restriction.</h2>
+                <h2 className="mb-1.5 leading-normal">Anywhere, anytime, for free, no restrictions.</h2>
                 <Link to="/Movies"><input className="oplusbuttons leading-loose mt-4" type="button" value="Access As Guest"/></Link>
             </div>
             </body>

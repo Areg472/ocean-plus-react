37c470039de9a7d65dc4be51089315c1af8ae36b
Areg472 <aregazatyank@outlook.com>
2024-10-15 12:19:41 +0400

    Hash router --> Browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {HashRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import {Homepage} from './pages/homepage.tsx';
 import {Movies} from './pages/movies.tsx';
 import {Layout} from "./Layout.tsx";

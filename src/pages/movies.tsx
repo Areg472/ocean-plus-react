e46f6d2a909dde8cbeb2a8de227276569c5aa044
Areg472 <aregazatyank@outlook.com>
2024-10-16 09:56:25 +0400

    movies?

--- src/pages/movies.tsx
+++ src/pages/movies.tsx
@@ -14,6 +14,17 @@ export function Movies() {
             <body>
                 <div className="Headings">
                     <h1 className="leading-normal">Movies and shows!</h1>
+                    <div className="row-1">
+                        <div className="col-1">
+                            <img src=""/>
+                        </div>
+                        <div className="col-2">
+                            <img src=""/>
+                        </div>
+                        <div className="col-3">
+                            <img src=""/>
+                        </div>
+                    </div>
                 </div>
             </body>
         </>

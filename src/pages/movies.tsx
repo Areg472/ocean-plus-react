6a635b381a602c540427e0dc20d1bdb6dee1d609
Areg472 <aregazatyank@outlook.com>
2024-10-17 09:52:46 +0400

    movies?

--- src/pages/movies.tsx
+++ src/pages/movies.tsx
@@ -14,15 +14,15 @@ export function Movies() {
             <body>
                 <div className="Headings">
                     <h1 className="leading-normal">Movies and shows!</h1>
-                    <div className="row-1">
+                    <div className="row-1 space-x-2.5 mt-2">
                         <div className="col-1">
-                            <img src=""/>
+                            <img src="https://github.com/Areg472/ocean-plus-react/blob/main/src/pictures/Cailou-the-movie-thumbnail00086400.png?raw=true" alt="Caillou movie"/>
                         </div>
                         <div className="col-2">
-                            <img src=""/>
+                            <img src="https://github.com/Areg472/ocean-plus-react/blob/main/src/pictures/CFTWCFTL-The-Movie-ocean-poster-with-the-template00086400.png?raw=true" alt="cftwcftl movie 123"/>
                         </div>
                         <div className="col-3">
-                            <img src=""/>
+                            <img src="https://github.com/Areg472/ocean-plus-react/blob/main/src/pictures/CFTWCFTL-The-Movie-4-ocean-poster-with-the-template00086400.png?raw=true" alt="cftwcftl movie 4"/>
                         </div>
                     </div>
                 </div>

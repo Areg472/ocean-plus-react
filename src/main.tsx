6eb097b8e787936389e3e2e3dbae2f190fb41cf1
Areg472 <aregazatyank@outlook.com>
2024-10-25 22:12:13 +0400

    vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import App from './App.tsx'
 import './index.css'
 import { HelmetProvider } from 'react-helmet-async';
 import {StrictMode} from "react";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
+        <SpeedInsights />
         <HelmetProvider>
             <App />
         </HelmetProvider>

9acc9c1f72c7c32c9bfbbd2fa42453e38bd97363
Aregus <74340447+Areg472@users.noreply.github.com>
2024-10-29 12:18:33 +0400

    nothing

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -18,7 +18,10 @@ export function Homepage() {
             <div className="Headings">
                 <h1 className="mb-4 leading-normal">Vyond movies and shows.</h1>
                 <h2 className="mb-1.5 leading-normal">Anywhere, anytime, for free, no restrictions.</h2>
-                <Link to="/Movies"><input className="oplusbuttons leading-loose mt-4" type="button" value="Access As Guest"/></Link>
+                <div className="buttonscont space-x-3">
+                    <Link to="/Movies"><input className="oplusbuttons leading-loose mt-4" type="button" value="Access the website"/></Link>
+                    <Link to="/Privacy-policy"><input className="oplusbuttons leading-loose mt-4" type="button" value="Privacy Policy"/></Link>
+                </div>
             </div>
             </body>
             <Footer/>

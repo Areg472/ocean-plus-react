0ec10e9270f859c7b6134362e0d3968aa18bf371
Aregus <74340447+Areg472@users.noreply.github.com>
2024-11-04 18:39:51 +0400

    nothing

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import vike from 'vike/plugin'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vike({ prerender: true })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

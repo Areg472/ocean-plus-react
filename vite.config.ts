b22158e0afd98bd751ba49e5cfa0dd20a2f0d0ca
Aregus <74340447+Areg472@users.noreply.github.com>
2024-11-05 19:20:11 +0400

    testh

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
-  plugins: [react(), vercel()],
-  vercel: {
-
-  },
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

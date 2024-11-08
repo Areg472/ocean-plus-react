17a3b34b8e8208c9365419f77c11fae45311d328
Aregus <74340447+Areg472@users.noreply.github.com>
2024-11-08 17:56:41 +0400

    weird ahh moment

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,11 @@ import App from './App.tsx'
 import './index.css'
 import { HelmetProvider } from 'react-helmet-async';
 import {StrictMode} from "react";
-import { SpeedInsights } from '@vercel/speed-insights/react';
 import {DevSupport} from "@react-buddy/ide-toolbox";
 import {ComponentPreviews, useInitial} from "@/dev";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <SpeedInsights />
         <HelmetProvider>
             <DevSupport
                 ComponentPreviews={ComponentPreviews}

c5a3c9cad222fe4167b81a553ee0312352299680
Aregus <74340447+Areg472@users.noreply.github.com>
2024-11-12 22:01:05 +0400

    changed aboutus.tsx with a link

--- src/pages/aboutus.tsx
+++ src/pages/aboutus.tsx
@@ -37,9 +37,9 @@ export function Aboutus() {
                                 <p>Main developer</p>
                             </CardaregHeader>
                             <CardaregContent>
-                                <a href="https://www.youtube.com/@aregthevideoeditor" className="ml-0 md:ml-4" target="_blank">
+                                <a href="https://areg.oceanbluestream.com/" className="ml-0 md:ml-4" target="_blank">
                                     <button className="button w-24">
-                                        Youtube
+                                        My page
                                     </button>
                                 </a>
                             </CardaregContent>

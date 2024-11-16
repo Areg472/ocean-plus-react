a863e032ec9b272e293f8e4a779f49ce9f6feafa
Aregus <74340447+Areg472@users.noreply.github.com>
2024-11-16 22:24:01 +0400

    adding logo properly

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,8 +7,9 @@ export function Navbar() {
         <>
             <div className="navbar">
                 <Link to="/">
-
-                        <img src="https://raw.githubusercontent.com/Areg472/ocean-plus-react/refs/heads/main/src/pictures/logoresizedu.jpg" className="leading-loose image" alt="logo"/>
+                    <div className="leading-loose image">
+                        <img src="/src/pictures/oplus.svg" className="actualimage" alt="logo"/>
+                    </div>
                 </Link>
                 <Link to="/Movies">
                     <button className="button leading-loose">Movies</button>

ca2d14e05b84750dc9eb6faa190d03f9528decca
Aregus <74340447+Areg472@users.noreply.github.com>
2024-12-03 18:25:17 +0400

    hmmm

--- src/pages/privacypolicy.tsx
+++ src/pages/privacypolicy.tsx
@@ -13,23 +13,26 @@ export function Privacy() {
             />
             <Navbar/>
             <body>
-                <div className="container">
-                    <p className="text-2xl leading-loose">
-                        By using Ocean+ your session data will be collected by Heap.
-                    </p>
-                    <p className="text-2xl leading-loose">
-                        We don't sell your data to third party companies.
-                    </p>
-                    <p className="text-2xl leading-loose">
-                        By visiting any Ocean+ page your request info, Network service provider, Postal code(in some
-                        countries), User Agent, IP address and request codes will be stored on a logging service for 3
-                        days before deleted.
-                    </p>
-                    <p className="text-2xl leading-loose">
-                        By visiting anything on Ocean+ for more than 30 seconds then your session will be recorded by
-                        using Hotjar.
-                    </p>
-                </div>
+            <div className="container mt-4 mb-4">
+                <p className="text-2xl leading-loose">
+                    By using Ocean+ your session data will be collected by Heap.
+                </p>
+                <p className="text-2xl leading-loose">
+                    We don't sell your data to third party companies.
+                </p>
+                <p className="text-2xl leading-loose">
+                    By visiting any Ocean+ page your request info, Network service provider, Postal code(in some
+                    countries), User Agent, IP address and request codes will be stored on a logging service for 3
+                    days before deleted.
+                </p>
+                <p className="text-2xl leading-loose">
+                    By visiting anything on Ocean+ for more than 30 seconds then your session will be recorded by
+                    using Hotjar.
+                </p>
+                <p className="text-2xl leading-loose">
+                    By visiting anything on Ocean+ your data will be recorded by Datadog for website load speed measurement.
+                </p>
+            </div>
             </body>
             <Footer/>
         </>

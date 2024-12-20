7931cb0af982f4d574b2b689a4fbee6836cb650f
Aregus <74340447+Areg472@users.noreply.github.com>
2024-12-20 10:39:21 +0400

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -10,13 +10,9 @@ import { datadogRum } from '@datadog/browser-rum';
 datadogRum.init({
     applicationId: '37e3dcd2-6459-48f8-96c4-1292a1aab76a',
     clientToken: 'pub027789559e0e7b3b2407604a07a2edbb',
-    // `site` refers to the Datadog site parameter of your organization
-    // see https://docs.datadoghq.com/getting_started/site/
     site: 'us5.datadoghq.com',
     service: 'ocean+',
     env: '<ENV_NAME>',
-    // Specify a version number to identify the deployed version of your application in Datadog
-    // version: '1.0.0',
     sessionSampleRate: 100,
     sessionReplaySampleRate: 20,
     trackUserInteractions: true,
